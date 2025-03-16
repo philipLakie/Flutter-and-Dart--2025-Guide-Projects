7821ccd2c743f2c5f9b3f1ea972befe20928fbe6
Lakie <philipabelflorida@gmail.com>
2025-03-16 08:44:47 +0800

    final changes to grocery app

---
+++ shopping_list_app/lib/models/category.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  fish,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other,
+}
+
+class Category {
+  final String title;
+  final Color color;
+
+  const Category(this.title, this.color);
+}
